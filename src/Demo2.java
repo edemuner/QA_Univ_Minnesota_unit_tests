3cd821191f20d01fcb876822a98518b922fa4bd5
edemuner <eduardood15@hotmail.com>
2023-02-06 22:20:03 -0300

    junit homework

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Demo2 {
+import java.util.Scanner;
+
+public class Demo2 {
+
+    public static void main(String[] args){
+
+        double r, area, circum;
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter the radius: ");
+        r = reader.nextDouble();
+
+        circum = 2 * Math.PI * r;
+        area = Math.PI * Math.pow(r, 2);
+
+        System.out.println("For a circle with radius " + r + ",");
+        System.out.println(" the circumference is " + circum);
+        System.out.println(" and the area is " + area + ".");
+
+        reader.close();
+    }
 }
--- src/Demo2Test.java
+++ src/Demo2Test.java
@@ -1,2 +1,128 @@
-package PACKAGE_NAME;public class Demo2Test {
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Before;
+import org.junit.Test;
+public class Demo2Test {
+
+    @Test
+    public void mainTestInput1(){
+        String input = "1\n";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        // invoke
+        String[] args = {};
+        Demo2.main(args);
+
+        //expected output
+        String consoleOutput = "Enter the radius: " + System.getProperty("line.separator");
+        consoleOutput += "For a circle with radius 1.0," + System.getProperty("line.separator");
+        consoleOutput += " the circumference is 6.283185307179586" + System.getProperty("line.separator");
+        consoleOutput += " and the area is 3.141592653589793." + System.getProperty("line.separator");
+
+        assertEquals(consoleOutput, out.toString());
+    }
+
+    //radius 3
+    @Test
+    public void mainTestInput3(){
+        String input = "3\n";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        // invoke
+        String[] args = {};
+        Demo2.main(args);
+
+        //expected output
+        String consoleOutput = "Enter the radius: " + System.getProperty("line.separator");
+        consoleOutput += "For a circle with radius 3.0," + System.getProperty("line.separator");
+        consoleOutput += " the circumference is 18.84955592153876" + System.getProperty("line.separator");
+        consoleOutput += " and the area is 28.274333882308138." + System.getProperty("line.separator");
+
+        assertEquals(consoleOutput, out.toString());
+    }
+
+
+    //radius 0
+    @Test
+    public void mainTestInput0(){
+        String input = "0\n";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        // invoke
+        String[] args = {};
+        Demo2.main(args);
+
+        //expected output
+        String consoleOutput = "Enter the radius: " + System.getProperty("line.separator");
+        consoleOutput += "For a circle with radius 0.0," + System.getProperty("line.separator");
+        consoleOutput += " the circumference is 0.0" + System.getProperty("line.separator");
+        consoleOutput += " and the area is 0.0" + System.getProperty("line.separator");
+
+        assertEquals(consoleOutput, out.toString());
+    }
+
+
+    //radius -1, error
+    @Test
+    public void mainTestInputMinus1(){
+        String input = "-1\n";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        // invoke
+        String[] args = {};
+        Demo2.main(args);
+
+        //expected output
+        String consoleOutput = "Error - You should provide a positive number" + System.getProperty("line.separator");
+
+        assertEquals(consoleOutput, out.toString());
+    }
+
+
+    // radius "abc"
+    @Test
+    public void mainTestInputAbc(){
+        String input = "abc";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        // invoke
+        String[] args = {};
+        Demo2.main(args);
+
+        //expected output
+        String consoleOutput = "Error - You should provide a positive number" + System.getProperty("line.separator");
+
+        assertEquals(consoleOutput, out.toString());
+    }
+
+
 }
